cae12c9c26c60fe8e92e746fc9c5374ced96445d
deveraj <deveraj@wwu.edu>
2019-10-30 11:46:46 -0700

    inital version

--- Room.java
+++ Room.java
@@ -1,3 +1,33 @@
+/**
+ * Jorell de Vera
+ * Zack Doll
+ * 2019
+ */
+
+ /**
+  * Room
+  */
 public class Room {
-    
+    private String name;
+    private int[] players;
+    private String[] neighbors;
+    private RoleSpot[] roleSpots;
+
+    /**
+     * addPlayer
+     * 
+     * adds a player to the room
+     */
+    public void addPlayer() {
+
+    }
+
+    /**
+     * removePlayer
+     * 
+     * removes a player from the room
+     */
+    public void removePlayer() {
+
+    }
 }
