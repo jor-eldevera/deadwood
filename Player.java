a5abce8ceff0d59480c1a216c47ceb80e075c4e0
Zack Doll <dollz@wwu.edu>
2019-10-30 11:54:17 -0700

    wrote player and die skeletons. Then going to work on Board

--- Player.java
+++ Player.java
@@ -1,3 +1,63 @@
 public class Player {
+    private int ID;
+    private Die rank;
+    private int money = 0;
+    private int credits = 0;
+    private int practice = 0;
+    private RoleSpot role;
+    private Boolean isTurn;
+    private Room curRoom;
+
     
+    public void setID(int ID) {
+
+    }
+
+    public int getID() {
+        return ID;
+    }
+    
+    public void setMoney (int change) {
+    
+    }
+
+    public void setCredits(int change) {
+
+    }
+
+    public void changeTurn() {
+        isTurn = !isTurn;
+    }
+
+    public boolean isTurn() {
+        return isTurn;
+    }
+
+    public void setPractice(int change) {
+
+    }
+
+    public void rehearse() {
+        setPractice(practice+1);
+    }
+
+    public void upgradeRank(int money, int credits) {
+
+    }
+
+    public void move(Room newRoom) {
+
+    }
+
+    public void takeRole(RoleSpot newRole) {
+
+    }
+
+    public void rollDice() {
+
+    }
+
+    public void resetPractice() {
+        setPractice(0);
+    }
 }
