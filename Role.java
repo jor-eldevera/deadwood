049dea9c4cd3ba12134b0a47a02d2a87e7a86b7d
deveraj <deveraj@wwu.edu>
2019-10-31 12:40:24 -0700

    initial skeleton

---
+++ Role.java
@@ -0,0 +1,46 @@
+/**
+ * Jorell de Vera
+ * Zack Doll
+ * Fall 2019
+ */
+
+public class RoleSpot {
+	private String spotID;
+	private int rank;
+	private boolean occupied;
+
+	/**
+	 * isOccupied
+	 * 
+	 * @return whether the Role Spot is occupied
+	 */
+	public boolean isOccupied() {
+
+	}
+
+	/**
+	 * flipOccupied
+	 * 
+	 * switches the state of occupied
+	 * @return the state of occupied AFTER the flip occurs
+	 */
+	public void flipOccupied() {
+
+	}
+
+	public void setRank(int rank) {
+
+	}
+
+	public int getRank() {
+
+	}
+
+	public void setID(String id) {
+
+	}
+
+	public String getID() {
+
+	}
+}
--- SceneCard.java
+++ SceneCard.java
@@ -1,3 +1,59 @@
+/**
+ * Jorell de Vera
+ * Zack Doll
+ * Deadwood
+ * Fall 2019
+ */
+
+/**
+ * Scene Card
+ */
 public class SceneCard {
-    
+    private String name;
+    private String desc;
+    private int budget;
+    private RoleSpot[] roles;
+	private boolean isFlipped;
+
+	/**
+	 * flip
+	 * 
+	 * flips the card
+	 * @return state of the card AFTER the flip occurs
+	 */
+	public boolean flip() {
+
+	}
+
+	public void setName(String name) {
+
+	}
+
+	public String getName() {
+
+	}
+
+	public void setDesc(String desc) {
+
+	}
+
+	public String getDesc() {
+
+	}
+
+	public void setBudget(int budget) {
+
+	}
+
+	public int getBudget() {
+
+	}
+
+	public void setRoles(Rolespot[] roles) {
+
+	}
+
+	public RoleSpot[] getRoles() {
+
+	}
 }
