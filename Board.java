d92c014a3da58ace5850e5bca850debd9cdcef5b
Zack Doll <dollz@wwu.edu>
2019-11-01 11:49:02 -0700

    wrote skeleton of Board class

--- Board.java
+++ Board.java
@@ -1,4 +1,50 @@
+import java.util.*;
+
 public class Board {
-    private Room rooms[] = new Room[12]; //need to abstract the data structure!
+    
+    private Collection<Room> rooms = new ArrayList<Room>(); //is this a useful abstraction? Not sure where the actual implementation would happen
+    
+    /*
+    pMoveOpts. Take a Player and use its curRoom to find the Room's
+    neighbors. Print these neighbors to the user.
+    */
+    public void pMoveOpts(Player p) {
+    }
+    
+    /*
+    addRoom. Add newRoom to rooms Collection (ArrayList)
+    */
+    public void addRoom(Room newRoom) {
+    }
+
+
+    /*
+    placeScene. Place newScene in a Set (extends room) that has no SceneCard.
+    */
+    public void placeScene(SceneCard newScene) {
+    }
+
+
+    /*
+    rmvScene. Remove SceneCard from set
+    */
+    public void rmvScene(Set set) {
+    }
+
+
+    /*
+    flipScene. Flip over SceneCard of set.
+    */
+    public void flipScene(Set set) {
+    }
+
+    
+    /*
+    placeShotCounter. Add shot counter to Set.
+    */
+    public void placeShotCounter(Set set) {
+    }
+
+
     
 }
